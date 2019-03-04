3be94c580e4ef776d2d070741c38275fe44db897
T-Rekt <t.to.the.rekt@gmail.com>
2019-03-04 23:27:49 +0700

    Remove default jar

--- index.js
+++ index.js
@@ -17,8 +17,7 @@ request = request.defaults({
   },
   headers: {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
-  },
-  jar: true
+  }
 });
 
 init = (options = {}) => {
